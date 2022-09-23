f612d9e037d251a4f4e554ce5769e500a0dd95b2
jlewisit <joelewis.mortgage@gmail.com>
2022-09-23 10:23:47 -0700

    model drafts

--- Develop/models/Tag.js
+++ Develop/models/Tag.js
@@ -1,12 +1,23 @@
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
+const { findAll } = require('./Product');
 
 class Tag extends Model {}
 
 Tag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
     sequelize,
